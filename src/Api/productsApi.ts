7fc6078d7ad1904593f4165d219df963c6d07e45
farid <infraestructura-switchs-movete@hotmail.com>
2025-07-20 22:40:43 -0500

    feat: actualizacion de URL

--- src/Api/productsApi.ts
+++ src/Api/productsApi.ts
@@ -2,7 +2,7 @@ import type { ProductType } from '../types/productsType';
 
 
 export async function getProductsByCompany(): Promise<ProductType[]> {  // Cambié 'Product[]' por 'ProductType[]'
-  const response = await fetch(' https://arqmv-module-back-whatsapp-qr-app-backend.onrender.com/product/getProductByCompany/238'); // Agregué 'http://'
+  const response = await fetch(' https://arqmv-module-back-whatsapp-qr-app-backend.onrender.com/api/back-whatsapp-qr-app/product/getProductByCompany/238'); // Agregué 'http://'
 
   if (!response.ok) {
     throw new Error('Error fetching products');
